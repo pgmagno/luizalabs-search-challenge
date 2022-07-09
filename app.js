1317db297b7e02a6238d0f355ae1826d535d19a9
Paulo Giovanny <pgs.magno@gmail.com>
2022-07-09 12:11:44 -0300

    turns file reading functions synchronous

--- app.js
+++ app.js
@@ -7,31 +7,23 @@ const print = require('./printResults');
 
 const path = "./data/";
 
-fs.readdir(path, (err, files) => {
-    if (err)
-        console.log(err);
-    else {
+const files = fs.readdirSync(path, 'utf-8');
+const sortedFiles = Array.from(files).sort( (last, next) => last > next ? 1 : -1);
         
-        const sortedFiles = Array.from(files).sort( (last, next) => last > next ? 1 : -1);
-        
-        const terms = p.receiveUserInput(process.argv[2]);  
-        const termsAndRules = p.prepareTerms(terms, r.rules);
-        let count = 0;
+const terms = p.receiveUserInput(process.argv[2]);
+console.log(terms);
+return
+const termsAndRules = p.prepareTerms(terms, r.rules);
 
-        sortedFiles.forEach(fileName => {
-            
-            const data = fs.readFileSync(path + fileName, 'utf8');
-            termsAndRules.forEach(rule => {
-                if(s.searchForTerms(rule[1], rule[2], data)){
-                    rule[3].push(fileName);
-                }                
-            });
-            count++;
-
-            if(count == sortedFiles.length) {
-                const finalReport = remove.removeOuterDuplicates(termsAndRules);    
-                print.printReport(finalReport);
-            }
-        });
-    }    
+sortedFiles.forEach(fileName => {
+    
+    const data = fs.readFileSync(path + fileName, 'utf8');
+    termsAndRules.forEach(rule => {
+        if(s.searchForTerms(rule[1], rule[2], data)){
+            rule[3].push(fileName);
+        }                
+    });
 });
+const finalReport = remove.removeOuterDuplicates(termsAndRules);    
+print.printReport(finalReport);
+        
