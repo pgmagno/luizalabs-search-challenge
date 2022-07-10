505bf563ad05eacb600df92ed487655fba793a6e
pgmagno <pgs.magno@gmail.com>
2022-07-10 17:20:05 -0300

    changes printResults.js to accept userInput and path variables

--- app.js
+++ app.js
@@ -6,26 +6,25 @@ const remove = require('./removeDuplicates');
 const print = require('./printResults');
 const c = require('./readAndPrepareDir');
 
-// begin execution
 const path = process.argv[3];
-const rawInput = process.argv[2];
+const userInput = process.argv[2];
 
 try {
     p.checkRules(r.rules);
     const files = c.readAndPrepareDir(path);    
-    const terms = p.receiveUserInput(rawInput);        
+    const terms = p.receiveUserInput(userInput);        
     const termsAndRules = p.prepareTerms(terms, r.rules);
     
     files.forEach(fileName => {        
-        const data = fs.readFileSync(path + fileName, 'utf8');
+        const fileContents = fs.readFileSync(path + fileName, 'utf8');
         termsAndRules.forEach(rule => {
-            if(s.searchForTerms(rule[1], rule[2], data)){
+            if(s.searchForTerms(rule[1], rule[2], fileContents)){
                 rule[3].push(fileName);
             }    
         });
     });
     const finalReport = remove.removeOuterDuplicates(termsAndRules);    
-    print.printReport(finalReport);
+    print.printReport(userInput, path, finalReport);
 } catch (e) {
     console.log();
     console.error(e);
--- printResults.js
+++ printResults.js
@@ -1,15 +1,15 @@
-function printReport(report) {
+function printReport(input, path, report) {
 
     report.forEach(entry => {
         if(entry[3].length == 0) {
-            console.log(`\nPara a Regra "${entry[0]}", não foram encontradas ocorrências para o termo "${process.argv[2]}".`);
+            console.log(`\nPara a Regra "${entry[0]}", não foram encontradas ocorrências para o termo "${input}" no diretório "${path}".`);
         } else {
             if(entry[3].length == 1) {
-                console.log(`\nPara a Regra "${entry[0]}" foi encontrada ${entry[3].length} ocorrência para o termo "${process.argv[2]}".`);
-                console.log(`O arquivo que possui "${process.argv[2]}" é:\n`)    
+                console.log(`\nPara a Regra "${entry[0]}" foi encontrada ${entry[3].length} ocorrência para o termo "${input}" no diretório "${path}".`);
+                console.log(`O arquivo que possui "${input}" é:\n`)    
             } else {            
-                console.log(`\nPara a Regra "${entry[0]}" foram encontrados ${entry[3].length} ocorrências para o termo "${process.argv[2]}".`);
-                console.log(`Os arquivos que possuem "${process.argv[2]}" são:\n`)
+                console.log(`\nPara a Regra "${entry[0]}" foram encontrados ${entry[3].length} ocorrências para o termo "${input}" no diretório "${path}".`);
+                console.log(`Os arquivos que possuem "${input}" são:\n`)
             }
             entry[3].forEach( result => console.log("\t" + result));
         }
